98c0cf2a0c4133a92d61ef597982a75c9c346dcb
Andrew Pham <andrewpham@Mac.lan>
2026-06-22 01:19:49 -0700

    fix: allow all CORS origins for Railway API

--- server/main.py
+++ server/main.py
@@ -14,8 +14,7 @@ app = FastAPI(title="Starboard API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
